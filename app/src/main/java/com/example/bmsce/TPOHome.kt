737f800e746d483cb3553ce766550c7ada517973
Nidhi S Adiga <nidhi.is21@bmsce.ac.in>
2024-02-10 15:47:55 +0530

    Added logout in TPO

--- app/src/main/java/com/example/bmsce/TPOHome.kt
+++ app/src/main/java/com/example/bmsce/TPOHome.kt
@@ -2,9 +2,15 @@ package com.example.bmsce
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bmsce.databinding.TpoHomeBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class TPOHome : AppCompatActivity(){
     private lateinit var binding: TpoHomeBinding
@@ -17,9 +23,11 @@ class TPOHome : AppCompatActivity(){
         firebaseAuth = FirebaseAuth.getInstance()
 
         // Set click listener for the logout button
-        binding.logoutbtn.setOnClickListener {
+        binding.logoutbtn1.setOnClickListener {
             // Sign out the current user
             firebaseAuth.signOut()
+
+            Toast.makeText(this, "You have been logged out", Toast.LENGTH_SHORT).show()
 
             // Start the InitialHomeScreenActivity and finish the current activity
             startActivity(Intent(this@TPOHome, MainActivity::class.java))
