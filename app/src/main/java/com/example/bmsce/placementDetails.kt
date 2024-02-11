67f4bdcc6a99990d236872e07cc53219ed382ce9
Jethra <jethra.is21@bmsce.ac.in>
2024-02-11 18:02:45 +0530

    Placement details page created

--- app/src/main/java/com/example/bmsce/placementDetails.kt
+++ app/src/main/java/com/example/bmsce/placementDetails.kt
@@ -1,4 +1,12 @@
-package com.example.bmsce
+package com.example.bmsce;
 
-class placementDetails{
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class placementDetails: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.placement_details)
+    }
 }
